54d1c89c648ea6d3e0034cd6618cace75cbfb684
Faolan C-P <fcheslack@gmail.com>
2016-06-08 18:56:47 -0400

    split into both theme and plugin
    for the tiny subset of non-overlapping functionality

---
+++ theme/about.php
@@ -0,0 +1,11 @@
+<?php if (!defined('APPLICATION')) exit();
+/**
+ * An associative array of information about this application.
+ */
+$ThemeInfo['default'] = array(
+   'Name' => 'zotero.org',
+   'Description' => "Theme used for zotero.org forums. Built from the default vanilla theme (distributed with Vanilla 2).",
+   'Version' => APPLICATION_VERSION,
+   'Author' => "Faolan Cheslack-Postava",
+   'AuthorEmail' => 'fcheslack@zotero.org',
+);
