f472fd8e28765697967f0b15f5df775866adb2ea
Faolan C-P <fcheslack@gmail.com>
2016-06-14 14:23:31 -0400

    remove debug alert

--- theme/js/moderation.js
+++ theme/js/moderation.js
@@ -17,39 +17,8 @@ jQuery(document).ready(function($) {
 		return IDs;
 	};
 
-	/*
-	var afterSuccess = function(data) {
-		// Figure out the IDs that are currently in the view.
-		var rows = [];
-		$('#Log tbody tr').each(function(index, element) {
-			if ($(element).attr('id') == 'SelectAll')
-				return;
-			rows.push($(element).attr('id'));
-		});
-		var rowsSelector = '#' + rows.join(',#');
-
-		// Requery the view and put it in the table.
-		$.get(
-			window.location.href,
-			{'DeliveryType': 'VIEW'},
-			function(data) {
-				$('#LogTable').html(data);
-				setExpander();
-
-				// Highlight the rows that are different.
-				var $foo = $('#Log tbody tr').not(rowsSelector);
-
-				$foo.effect('highlight', {}, 'slow');
-			});
-
-		// Update the counts in the sidepanel.
-		$('.Popin').popin();
-	};
-	*/
-
 	$('.SpammerButton').click(function(e) {
 		e.preventDefault();
-		alert('spammerButton clicked');
 		var IDs = getIDs().join(',');
 		$.ajax(
 			gdn.url('/zotero/spammer?logids=' + IDs),
